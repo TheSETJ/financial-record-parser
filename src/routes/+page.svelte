9ed3853b4a6615f8679c30dce01d63426f1c9cd4
Sayyed Ehsan Taheri Javid <ehsantaheri25@yahoo.com>
2023-12-12 16:27:06 +0330

    style: use spaces for indentation in all files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,114 +1,117 @@
 <script lang="ts">
-    let inputValue: string = '';
-    let parsedValues: Array<string> = [];
+  let inputValue: string = '';
+  let parsedValues: Array<string> = [];
 
-    function parse(record: string): string {
-        record = parseVerb(record);
+  function parse(record: string): string {
+    record = parseVerb(record);
 
-        record = replaceAmount(record);
+    record = replaceAmount(record);
 
-        return record;
+    return record;
+  }
+
+  function parseVerb(record: string): string {
+    const verbs = ['پرداخت', 'دریافت', 'جابه‌جایی'];
+
+    let hasVerb = verbs.some((verb) => record.startsWith(verb));
+
+    if (!hasVerb) {
+      record = verbs[0] + ' ' + record;
     }
 
-    function parseVerb(record: string): string {
-        const verbs = ['پرداخت', 'دریافت', 'جابه‌جایی'];
+    return record;
+  }
 
-        let hasVerb = verbs.some((verb) => record.startsWith(verb));
+  function replaceAmount(record: string): string {
+    const amountRegex =
+      /[۰-۹](هزار|میلیون|میلیارد|ریال|تومان|[۰-۹]|\s|و)*(:?هزار|میلیون|میلیارد|ریال|تومان|[۰-۹]|\s|و)/g;
 
-        if (!hasVerb) {
-            record = verbs[0] + ' ' + record;
+    return record.replace(amountRegex, (match, ...groups) => {
+      const numericValue = parseAmount(match);
+      return numericValue.toString() + ' ریال ';
+    });
+  }
+
+  function parseAmount(amountString: string): any {
+    amountString = amountString.replace(/[۰-۹]/g, (digit) =>
+      String.fromCharCode(digit.charCodeAt(0) - 1728)
+    );
+
+    const parts = amountString.split(/\s+/);
+
+    const numericValues: number[] = [];
+
+    let rialPartAlert = false;
+
+    for (const part of parts) {
+      if (part === 'ریال') {
+        let numericPart = numericValues[numericValues.length - 1];
+        if (numericPart > 10) {
+          numericValues[numericValues.length - 1] = numericPart / 10;
         }
+        continue;
+      }
 
-        return record;
-    }
+      if (part === 'تومان') {
+        rialPartAlert = true;
+      }
 
-    function replaceAmount(record: string): string {
-        const amountRegex = /[۰-۹](هزار|میلیون|میلیارد|ریال|تومان|[۰-۹]|\s|و)*(:?هزار|میلیون|میلیارد|ریال|تومان|[۰-۹]|\s|و)/g;
+      if (part === 'و') {
+        continue;
+      }
 
-        return record.replace(amountRegex, (match, ...groups) => {
-            const numericValue = parseAmount(match);
-            return numericValue.toString() + ' ریال ';
-        });
-    }
-
-    function parseAmount(amountString: string): any {
-        amountString = amountString.replace(/[۰-۹]/g, (digit) => String.fromCharCode(digit.charCodeAt(0) - 1728));
-
-        const parts = amountString.split(/\s+/);
-
-        const numericValues: number[] = [];
-
-        let rialPartAlert = false;
-
-        for (const part of parts) {
-            if (part === 'ریال') {
-                let numericPart = numericValues[numericValues.length - 1];
-                if (numericPart > 10) {
-                    numericValues[numericValues.length - 1] = numericPart / 10;
-                }
-                continue;
-            }
-
-            if (part === 'تومان') {
-                rialPartAlert = true;
-            }
-
-            if (part === 'و') {
-                continue;
-            }
-
-            if (part.includes('میلیارد')) {
-                const numericPart = parseInt(part.replace('میلیارد', ''), 10);
-                if (!isNaN(numericPart)) {
-                    numericValues.push(numericPart * 1e10);
-                }
-                continue;
-            }
-
-            if (part.includes('میلیون')) {
-                const numericPart = parseInt(part.replace('میلیون', ''), 10);
-                if (!isNaN(numericPart)) {
-                    numericValues.push(numericPart * 1e7);
-                }
-                continue;
-            }
-
-            if (part.includes('هزار')) {
-                const numericPart = parseInt(part.replace('هزار', ''), 10);
-                if (!isNaN(numericPart)) {
-                    numericValues.push(numericPart * 1e4);
-                }
-                continue;
-            }
-
-            const numericPart = parseInt(part, 10);
-            if (!isNaN(numericPart)) {
-                if (rialPartAlert) {
-                    numericValues.push(numericPart);
-                } else {
-                    numericValues.push(numericPart * 10)
-                    rialPartAlert = true;
-                }
-            }
+      if (part.includes('میلیارد')) {
+        const numericPart = parseInt(part.replace('میلیارد', ''), 10);
+        if (!isNaN(numericPart)) {
+          numericValues.push(numericPart * 1e10);
         }
+        continue;
+      }
 
-        return numericValues.reduce((sum, value) => sum + value, 0);
+      if (part.includes('میلیون')) {
+        const numericPart = parseInt(part.replace('میلیون', ''), 10);
+        if (!isNaN(numericPart)) {
+          numericValues.push(numericPart * 1e7);
+        }
+        continue;
+      }
+
+      if (part.includes('هزار')) {
+        const numericPart = parseInt(part.replace('هزار', ''), 10);
+        if (!isNaN(numericPart)) {
+          numericValues.push(numericPart * 1e4);
+        }
+        continue;
+      }
+
+      const numericPart = parseInt(part, 10);
+      if (!isNaN(numericPart)) {
+        if (rialPartAlert) {
+          numericValues.push(numericPart);
+        } else {
+          numericValues.push(numericPart * 10);
+          rialPartAlert = true;
+        }
+      }
     }
 
-    function parseLines() {
-        parsedValues = inputValue.split('\n').map(record => parse(record));
-    }
+    return numericValues.reduce((sum, value) => sum + value, 0);
+  }
+
+  function parseLines() {
+    parsedValues = inputValue.split('\n').map((record) => parse(record));
+  }
 </script>
 
-<style>
-    @import '../../public/build/tailwind.css';
-</style>
-
 <div class="container">
-    <textarea rows="5" bind:value={inputValue} on:input={parseLines}></textarea>
-    <output class="paragraphs">
-        {#each parsedValues as parsedValue, index (index)}
-            <p>{parsedValue}</p>
-        {/each}
-    </output>
+  <textarea rows="5" bind:value={inputValue} on:input={parseLines}></textarea>
+  <output class="paragraphs">
+    {#each parsedValues as parsedValue, index (index)}
+      <p>{parsedValue}</p>
+    {/each}
+  </output>
 </div>
+
+<style>
+  @import '../../public/build/tailwind.css';
+</style>
